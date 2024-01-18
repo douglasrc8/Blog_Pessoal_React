aca6170ae11dad490823077adadf701b5b04f8cd
douglas <douglasrc1912@gmail.com>
2024-01-18 19:29:55 -0300

    Atualização

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -12,4 +12,5 @@ export const cadastrarUsuario = async(url: string, dados: Object, setDados: Func
 export const login = async(url: string, dados: Object, setDados: Function) =>{
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
+
 }
