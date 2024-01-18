71c38c8d6caf783d7460afdabb146dc45ba3b34b
douglas <douglasrc1912@gmail.com>
2024-01-18 19:12:49 -0300

    Cadastro e Login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,39 @@
+﻿import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+
 function Navbar() {
- 
-  
 
-  return (
-    <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
+    const navigate = useNavigate();
 
-            <div className='flex gap-4'>
-              <div className='hover:underline'>Postagens</div>
-              <div className='hover:underline'>Temas</div>
-              <div className='hover:underline'>Cadastrar tema</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
+    const { handleLogout} = useContext(AuthContext)
+
+    function logout(){
+
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/login')
+    }
+
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
+            
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Navbar
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,50 +2,59 @@ function Home() {
 
     return (
         <>
-            <div  className="
-            bg-indigo-900 
-            flex 
-            justify-center">
-                <div  className="
-                container 
-                grid 
-                grid-cols-2 
-                text-white">
+            <div id="container" className="
+                flex 
+                justify-center 
+                bg-indigo-900
+                ">
+                <div id="subcontainer" className="
+                    container 
+                    grid 
+                    grid-cols-2 
+                    text-white
+                    ">
                     <div id="texto" className="
-                    flex 
-                    flex-col 
-                    gap-4 
-                    items-center 
-                    justify-center 
-                    py-4">
+                        flex 
+                        flex-col 
+                        gap-4 
+                        items-center 
+                        justify-center 
+                        py-4
+                        ">
                         <h2 className="
-                        text-5xl
-                        font-bold
+                            text-5xl
+                            font-bold
                         ">Seja Bem Vindo!</h2>
                         <p className="text-xl">Expresse aqui os seus pensamentos e opiniões</p>
+
                         <div className="
-                        flex 
-                        justify-around 
-                        gap-4">
-                            <button className="
-                            rounded 
-                            bg-white 
-                            text-blue-800 
-                            py-2 
-                            px-4">Nova Postagem
-                            </button>
+                            flex 
+                            justify-around 
+                            gap-4
+                            ">
+                            <div className="
+                                rounded 
+                                text-white 
+                                border-white 
+                                border-solid 
+                                border-2 
+                                px-4 
+                                py-2
+                                ">
+                                Nova Postagem
+                            </div>
                         </div>
                     </div>
 
-                    <div id="imagem" className="flex justify-center">
+                    <div id="imagem" className="
+                        flex 
+                        justify-center
+                        ">
                         <img
                             src="https://i.imgur.com/VpwApCU.png"
                             alt="Imagem da Página Home"
                             className="w-2/3"
                         />
-                    </div>
-                    <div>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit dignissimos reiciendis dolores maiores corrupti sunt delectus pariatur quaerat optio, odio, laborum facilis. Culpa, assumenda qui eaque magnam aspernatur vero? Vero.
                     </div>
                 </div>
             </div>
